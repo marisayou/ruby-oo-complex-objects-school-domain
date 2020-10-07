c5569ad8ff4a403a5f95521b40efbb18b35df68e
marisayou <marisayou2017@u.northwestern.edu>
2020-10-06 17:33:28 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+    def initialize(roster)
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        if @roster.key?(grade) 
+            @roster[grade].push(name)
+        else
+            @roster[grade] = [name]
+        end
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        @roster.each do |grade, name_arr|
+            @roster[grade] = name_arr.sort
+        end
+    end
+end
